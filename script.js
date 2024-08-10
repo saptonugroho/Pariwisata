efaa3984ce2c299b285e5a5e063389f7ce63befa
sapto nugroho <saptonugroho06@gmail.com>
2024-08-11 01:58:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const userInfo = document.getElementById("user-info");
+
+function CloseAside() {
+  userInfo.style.display = "none";
+}
+
+function OpenAside() {
+  userInfo.style.display = "block";
+}
+
